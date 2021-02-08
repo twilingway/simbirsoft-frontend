0185529eeeddfb9af048a09f30982bbe53e3efc3
Aleksei Glazunov <993468@mail.ru>
2021-02-08 12:37:22 +0400

    add picker form

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import { Navbar } from './components/Navbar';
 
+import { PickerForm } from './components/PickerForm';
+
 const App: React.FC = () => {
   return (
     <>
       <Navbar />
       <div className="container">
-        <h1>Football statistics</h1>
+        <PickerForm />
       </div>
     </>
   );
